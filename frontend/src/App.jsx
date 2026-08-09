5d5e32ad54666462aad950d09167930d3703c053
abhishekshebbar15 <abhishekshebbar15@gmail.com>
2026-08-09 12:56:09 +0530

    Connect frontend to deploy backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -62,7 +62,7 @@ if (greetings[lowerMessage]) {
     setLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/chat", {
+      const response = await fetch("http://python-rag-chatbot.vercel.app/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
